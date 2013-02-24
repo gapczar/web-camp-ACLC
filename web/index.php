098a09972b0e249899f5a13bd4ad020b7a378430
GAPLabs <GAPLabs@Administrators-Mac-mini.local>
2013-02-24 09:52:38 +0800

    Yehey

--- web/index.php
+++ web/index.php
@@ -1,1 +1,36 @@
-<?php echo "Hello World!" ?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/Style/style.css">
+		<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+		<script src="/Scripts/script.js"></script>
+	</head>
+	<body>
+		<header>
+			<section class="top-section">
+				Mobile Apps 'R Us!
+			</section>
+			<nav>
+				<ul class="clearfix">	
+					<li><a href="Home.html">Home</a></li>
+					<li><a href="SignUp.html">Sign Up</a></li>
+					<li><a href="Login.html">Login</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div class="main clearfix">
+			<section class="content">
+				<article>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</article>
+			</section>
+			<aside class="side-bar">
+				<article>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</article>
+			</aside>
+		</div>
+		<footer class="clearfix">
+			<p>Copyright &copy 2013 ACLC</p>
+		</footer>
+	</body>
+</html>
