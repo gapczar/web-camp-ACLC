600b174091e1184ccb5506fe5e7b7b1c77bf9754
GAPLabs <gaplabs@GAP-Admins-Mac-mini-2.local>
2013-02-24 11:15:13 -0500

    yehey

--- web/UserAccounts.sql
+++ web/UserAccounts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 24, 2013 at 06:56 PM
+-- Generation Time: Feb 24, 2013 at 05:00 PM
 -- Server version: 5.5.25
 -- PHP Version: 5.4.4
 
@@ -11,7 +11,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `Hackaton`
+-- Database: `mysql`
 --
 
 -- --------------------------------------------------------
@@ -25,5 +25,6 @@ CREATE TABLE `UserAccounts` (
   `Username` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
+  `hasVoted` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
